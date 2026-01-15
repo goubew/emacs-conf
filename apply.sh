f4c4c478be54c72e2accfe8cd99bea37023af85c
goubew <goubew@users.noreply.github.com>
2026-01-15 17:38:17 -0500

    Consolidate packages files

--- apply.sh
+++ apply.sh
@@ -22,18 +22,11 @@ mkdir -p "$HOME/org"
 safe_link "${SCRIPT_DIR}/init.el" "$HOME/.emacs.d/init.el"
 safe_link "${SCRIPT_DIR}/early-init.el" "$HOME/.emacs.d/early-init.el"
 safe_link "${SCRIPT_DIR}/packages.el" "$HOME/.emacs.d/packages.el"
+safe_link "${SCRIPT_DIR}/packages" "$HOME/.emacs.d/packages"
 safe_link "${SCRIPT_DIR}/funs" "$HOME/.emacs.d/funs"
-safe_link "${SCRIPT_DIR}/app-launcher.el" \
-          "$HOME/.emacs.d/packages/app-launcher.el"
-safe_link "${SCRIPT_DIR}/my-simple-theme-base.el" \
-          "$HOME/.emacs.d/packages/my-simple-theme-base.el"
 safe_link "${SCRIPT_DIR}/my-charcoal-light-theme.el" \
           "$HOME/.emacs.d/my-charcoal-light-theme.el"
-safe_link "${SCRIPT_DIR}/my-theme-base.el" \
-          "$HOME/.emacs.d/packages/my-theme-base.el"
 safe_link "${SCRIPT_DIR}/my-gruvbox-light-theme.el" \
           "$HOME/.emacs.d/my-gruvbox-light-theme.el"
 safe_link "${SCRIPT_DIR}/my-gruvbox-dark-theme.el" \
           "$HOME/.emacs.d/my-gruvbox-dark-theme.el"
-safe_link "${SCRIPT_DIR}/snw-mode.el" \
-          "$HOME/.emacs.d/packages/snw-mode.el"
