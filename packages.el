004e4c0d9cdf97876bc2b7f24e0517a6f456bfb8
goubew <goubew@users.noreply.github.com>
2026-07-05 15:58:23 -0400

    Back to evil
    Just can't get the hang of meow

--- packages.el
+++ packages.el
@@ -205,11 +205,79 @@
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
+(use-package evil
+  :ensure t
+  :init
+  (setq
+   evil-default-state 'emacs
+   evil-disable-insert-state-bindings t
+   evil-insert-state-message nil
+   evil-normal-state-message nil
+   evil-motion-state-message nil
+   evil-visual-state-message nil
+   evil-replace-state-message nil
+   evil-operator-state-message nil
+   evil-respect-visual-line-mode t
+   evil-search-module 'evil-search
+   evil-ex-search-persistent-highlight nil
+   evil-mode-line-format '(after . mode-line-front-space)
+   evil-symbol-word-search t
+   evil-shift-width 2
+   evil-undo-system 'undo-redo
+   evil-want-C-d-scroll t
+   evil-want-C-i-jump t
+   evil-want-C-u-delete t
+   evil-want-C-u-scroll t
+   evil-want-C-w-delete t
+   evil-want-keybinding nil
+   evil-emacs-state-tag "E "
+   evil-insert-state-tag "I "
+   evil-motion-state-tag "M "
+   evil-normal-state-tag "N "
+   evil-visual-state-tag "V "
+   evil-replace-state-tag "R "
+   evil-operator-state-tag "O")
+  :config
+  (evil-set-initial-state 'prog-mode 'normal)
+  (evil-set-initial-state 'text-mode 'normal)
+  (evil-set-initial-state 'conf-mode 'normal)
+  (evil-set-initial-state 'help-mode 'emacs)
+  (evil-set-initial-state 'grep-mode 'emacs)
+  (load-file (concat user-emacs-directory "funs/evil-funs.el"))
+  (evil-mode)
+  :bind (:map evil-normal-state-map
+              ("SPC" . my-emulate-ctrl-c)))
+
+(use-package evil-anzu
+  :ensure t
+  :after (anzu evil))
+
+(use-package evil-numbers
+  :ensure t
+  :after evil
+  :config
+  ;; Avoid using C-a C-x to keep the default C-x bindings
+  (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+  (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt))
+
+(use-package evil-surround
+  :ensure t
+  :after evil
+  :config
+  (global-evil-surround-mode))
+
 (use-package exec-path-from-shell
   :if (eq system-type 'darwin)
   :ensure t
   :config
   (exec-path-from-shell-initialize))
+
+(use-package expand-region
+  :ensure t
+  :bind (("C-=" . er/expand-region)
+         (:map evil-visual-state-map
+               ("," . er/contract-region)
+               ("." . er/expand-region))))
 
 (use-package vc-fossil
   :ensure t
@@ -255,10 +323,13 @@
 
 (use-package key-chord
   :ensure t
-  :after meow
+  :after evil
+  :init
+  (setq key-cord-two-keys-delay 0.5
+        key-chord-in-macros t)
   :config
   (key-chord-mode 1)
-  (key-chord-define meow-insert-state-keymap "fd" 'meow-insert-exit))
+  (key-chord-define evil-insert-state-map "fd" 'evil-normal-state))
 
 (use-package ledger-mode
   :ensure t
@@ -297,21 +368,6 @@
   :config
   (add-hook 'gfm-mode-hook 'visual-line-mode)
   :mode ("\\.md\\'" . gfm-mode))
-
-(use-package meow
-  :ensure t
-  :init
-  (load-file (concat user-emacs-directory "funs/meow-funs.el"))
-  (setq
-   meow-keypad-self-insert-undefined nil
-   meow-use-clipboard t
-   meow-cursor-type-insert 'box)
-  :config
-  (dolist (entry '((helpful-mode . motion)
-                   (help-mode . motion)))
-    (push entry meow-mode-state-list))
-  (meow-setup)
-  (meow-global-mode 1))
 
 (use-package neotree
   :ensure t
